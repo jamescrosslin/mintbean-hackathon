1a51d4ff7a13a9b5687cd4a6fd0566c5bcde10e9
James Crosslin <jamesmscrosslin@gmail.com>
2021-08-01 15:44:51 -0700

    move CreateGame modal out of Game Div

--- client/src/components/Games.js
+++ client/src/components/Games.js
@@ -37,33 +37,35 @@ function Games() {
 
   return (
     (error && <Redirect to={errorRoutes[error.response.status] || '/error'} />) || (
-      <div className="wrap main--grid">
-        {!isLoading && games}
-        <button
-          onClick={() => setCreating((prevState) => !prevState)}
-          className="course--module course--add--module"
-        >
-          <span className="course--add--title">
-            <svg
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              viewBox="0 0 13 13"
-              className="add"
-            >
-              <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
-            </svg>
-            New Game
-          </span>
-        </button>
+      <>
+        <div className="wrap main--grid">
+          {!isLoading && games}
+          <button
+            onClick={() => setCreating((prevState) => !prevState)}
+            className="course--module course--add--module"
+          >
+            <span className="course--add--title">
+              <svg
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                viewBox="0 0 13 13"
+                className="add"
+              >
+                <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+              </svg>
+              New Game
+            </span>
+          </button>
+        </div>
         {creating && (
           <CreateGame
             refreshGames={refreshGames}
             dismissForm={() => setCreating((prevState) => !prevState)}
           />
         )}
-      </div>
+      </>
     )
   );
 }

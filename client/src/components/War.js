b0c2440a135c9861271e38afaa88ebb6cb2b1b94
James Crosslin <jamesmscrosslin@gmail.com>
2021-08-02 22:10:29 -0700

    add classes to War

--- client/src/components/War.js
+++ client/src/components/War.js
@@ -1,55 +1,55 @@
-import React from "react"
-import { useUserContext } from "../context/UserContext"
-import { useFetchData } from "../hooks"
-import images from "../img"
+import React from 'react';
+import { useUserContext } from '../context/UserContext';
+import { useFetchData } from '../hooks';
+import images from '../img';
 
 function War({ game }) {
-  const { user } = useUserContext()
+  const { user } = useUserContext();
   const { data, fetchData } = useFetchData({
     isLoading: true,
     data: null,
-    error: null
-  })
+    error: null,
+  });
 
   function handleReady() {
     fetchData({
       apiRoute: `/api/games/war/${game.id}/play`,
-      method: "GET",
-      isAuthRequired: true
-    })
+      method: 'GET',
+      isAuthRequired: true,
+    });
   }
   return (
     <>
-      {game.status !== "created" &&
+      {game.status !== 'created' &&
         game.gameplay.map((player) => (
-          <figure key={player.id}>
-            {player.event && <h3>{player.event}!</h3>}
+          <figure className="player--cards" key={player.id}>
+            {player.event && <h3 className="player--event">{player.event}!</h3>}
             {player.showCards.map((card, i) => (
               <img
+                className="card--image"
                 key={i}
-                src={img[card]}
+                src={images[card]}
                 alt={`Card belonging to ${player.name}: ${card}`}
               />
             ))}
-            <figcaption>
+            <figcaption className="player--name">
               {`${player.name}: ${player.showCards.join(', ')}`}
-              <span className={player.ready ? 'ready' : undefined}>{player.ready && 'Ready!'}</span>
+              <span className={player.ready ? 'ready' : undefined}>
+                {' '}
+                -- {player.ready && 'Ready!'}
+              </span>
             </figcaption>
             {player.id === user.id && (
               <>
-                <button
-                  onClick={handleReady}
-                  disabled={player.ready}
-                  className="ready--button"
-                >
-                  {player.ready ? "Waiting..." : "Ready"}
+                <button onClick={handleReady} disabled={player.ready} className="ready--button">
+                  {player.ready ? 'Waiting...' : 'Ready'}
                 </button>
               </>
             )}
           </figure>
         ))}
     </>
-  )
+  );
 }
 
-export default War
+export default War;
